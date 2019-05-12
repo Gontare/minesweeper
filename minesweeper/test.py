257ae7249a4bac88aada9fa73c84b47d76d04235
Weronika <wer.olejniczak@gmail.com>
2019-05-12 16:29:47 +0200

    fixes #1

--- minesweeper/test.py
+++ minesweeper/test.py
@@ -1,19 +1,18 @@
 from minesweeper.implementation import *
 
-start, goal = (0, 0), (7, 8)
-
-came_from, cost_so_far = a_star_search(diagram4, start, goal)
-draw_grid(diagram4, width=3, point_to=came_from, start=start, goal=goal)
+# came_from, cost_so_far = a_star_search(diagram4, start, goal)
+# draw_grid(diagram4, width=3, point_to=came_from, start=start, goal=goal)
 # print()
-draw_grid(diagram4, width=3, number=cost_so_far, start=start, goal=goal)
+# draw_grid(diagram4, width=3, number=cost_so_far, start=start, goal=goal)
 # print()
-draw_grid(diagram4, width=3, path=reconstruct_path(came_from, start=start, goal=goal))
+# draw_grid(diagram4, width=3, path=reconstruct_path(came_from, start=start, goal=goal))
 
 
 def goal(tilemap, bombcounter):
     x = 0
     y = 0
     z = 1
+
     for x in range(15):
         if z == 0:
             break
@@ -25,10 +24,11 @@ def goal(tilemap, bombcounter):
                 z = 0
             if z == 0:
                 break
+
     return x, y
 
 
 def spritemove(start, goal):
         came_from, cost_so_far = a_star_search(diagram4, start, goal)
-        # draw_grid(diagram4, width=3, path=reconstruct_path(came_from, start=start, goal=goal))
+        draw_grid(diagram4, width=3, path=reconstruct_path(came_from, start=start, goal=goal))
         return reconstruct_path(came_from, start=start, goal=goal)
