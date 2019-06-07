898b3973cef351f68cd29ae6453ae250b1940b49
Weronika <wer.olejniczak@gmail.com>
2019-06-07 21:38:14 +0200

    astar algorithm correction #4

--- minesweeper/environment/bomb.py
+++ minesweeper/environment/bomb.py
@@ -1,4 +1,23 @@
+import random
+
 from minesweeper.environment.node import Node
 
-# class Bomb(Node):
-#    def __init__(self):
+
+class Bomb(Node):
+    def __init__(self, x, y, direction, cost, timer):
+        super().__init__(x, y, direction, cost)
+        self.timer = timer
+
+    @staticmethod
+    def generate_bombs():
+        bombsPos = [(2, 2), (2, 4), (2, 8), (2, 10), (2, 12), (2, 14),
+                    (4, 2), (4, 4), (4, 8), (4, 10), (4, 12), (4, 14),
+                    (6, 2), (6, 4), (6, 8), (6, 10), (6, 12), (6, 14),
+                    (8, 2), (8, 4), (8, 8), (8, 10), (8, 12), (8, 14),
+                    (10, 2), (10, 4), (10, 8), (10, 10), (10, 12), (10, 14),
+                    (12, 2), (12, 4), (12, 8), (12, 10), (12, 12), (12, 14),
+                    (14, 2), (14, 4), (14, 8), (14, 10), (14, 12), (14, 14)
+                    ]
+        randomInt = random.randint(0, 41)
+
+        return bombsPos[randomInt]
--- minesweeper/environment/grid.py
+++ minesweeper/environment/grid.py
@@ -1,11 +1,13 @@
-import random
+import numpy as np
 import pygame as pg
+
+from minesweeper.environment.bomb import Bomb
 
 
 class Grid:
     def __init__(self):
         # display dimensions
-        self.TILESIZE = 40
+        self.TILE_SIZE = 40
         self.MAPHEIGHT = 15
         self.MAPWIDTH = 15
 
@@ -13,62 +15,86 @@ class Grid:
         self.WHITE = (255, 255, 255)
         self.BLACK = (0, 0, 0)
 
-        # floors accessible
+        # floors
         self.GROUND = 0
-        self.LANDMINE = 1
-        # not accessible
-        self.OBSTACLE = 2
-        self.DEMINED = 3
-        self.PATH = 4
-        self.START = 5
+        self.WALL = 1
+        self.DEMINED = 2
+        self.START = 3
+        self.LANDMINE_N = 4
+        self.LANDMINE_E = 5
+        self.LANDMINE_S = 6
+        self.LANDMINE_W = 7
 
-        self.floor = [self.GROUND, self.LANDMINE, self.OBSTACLE, self.DEMINED, self.PATH, self.START]
+        # list of types of flooring
+        self.floor = [self.GROUND, self.WALL, self.DEMINED, self.START,
+                      self.LANDMINE_N, self.LANDMINE_E, self.LANDMINE_S, self.LANDMINE_W]
 
         # dictionary linking textures with floors
         self.textures = {
             self.GROUND: pg.image.load('resources/ground.png'),
-            self.OBSTACLE: pg.image.load('resources/obstacle.png'),
-            self.LANDMINE: pg.image.load('resources/landmine.png'),
+            self.WALL: pg.image.load('resources/obstacle.png'),
             self.DEMINED: pg.image.load('resources/demined.png'),
-            self.PATH: pg.image.load('resources/path.png'),
-            self.START: pg.image.load('resources/start.png')
+            self.START: pg.image.load('resources/start.png'),
+            self.LANDMINE_N: pg.image.load('resources/landmine_n.png'),
+            self.LANDMINE_E: pg.image.load('resources/landmine_e.png'),
+            self.LANDMINE_S: pg.image.load('resources/landmine_s.png'),
+            self.LANDMINE_W: pg.image.load('resources/landmine_w.png')
         }
 
         # a list representing our tilemap
         self.tilemap = []
-        self.bombcounter = 0
+        self.bomb_counter = 0
 
         # creating a new drawing surface; add '50' in place of '0' to create bar
-        self.SURFACE = pg.display.set_mode((self.TILESIZE * self.MAPWIDTH, self.TILESIZE * self.MAPHEIGHT + 0))
+        self.SURFACE = pg.display.set_mode((self.TILE_SIZE * self.MAPWIDTH, self.TILE_SIZE * self.MAPHEIGHT + 0))
 
-    def wall_up(self):
-        # self.tilemap[x][y] = self.OBSTACLE
+        # grid list for wall generation
+        self.grid = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # column 1 reversed
+                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # column 2 reversed
+                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # column 3 reversed
+                         [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],  # column 4 reversed
+                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # and so on...
+                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+                         ])
 
-        for x in range(10, 11):
-            for y in range(3, 12):
-                self.tilemap[x][y] = self.OBSTACLE
+    def wall_up(self, x, y):
+        self.tilemap[x][y] = self.WALL
+
+    def place_bombs(self):
+        for x in range(9):
+            bomb = Bomb.generate_bombs()
+            self.tilemap[bomb[0]][bomb[1]] = self.LANDMINE
 
     def generate_tilemap(self):
-        # randomly generated flooring
-        for rw in range(self.MAPHEIGHT):
-            for cl in range(self.MAPWIDTH):
-                num = random.randint(0, 20)
-                if num < 18:
-                    self.tilemap[rw][cl] = self.GROUND
-                # elif 10 <= num < 13:
-                    # tile = self.OBSTACLE
-                else:
-                    self.tilemap[rw][cl] = self.LANDMINE
-                self.tilemap[0][0] = self.START
-                self.tilemap[14][14] = self.START
-                # self.wall_up()
+        # start and end
+        self.tilemap[0][0] = self.START
+        self.tilemap[14][14] = self.START
+
+        # call to method which shows bombs
+        self.place_bombs()
+
+        for x in range(0, 14):
+            for y in range(0, 14):
+                if self.grid[x][y] == 1:
+                    # call to wall_up method which generates bombs
+                    self.wall_up(x, y)
+
         return self.tilemap
 
     def count_bombs(self):
         for rw in range(self.MAPHEIGHT):
             for cl in range(self.MAPWIDTH):
                 if self.tilemap[rw][cl] == self.LANDMINE:
-                    self.bombcounter += 1
+                    self.bomb_counter += 1
 
     def set_tilemap(self):
         self.tilemap = [[self.GROUND for w in range(self.MAPWIDTH)] for h in range(self.MAPHEIGHT)]
@@ -78,7 +104,7 @@ class Grid:
         for row in range(self.MAPHEIGHT):
             for column in range(self.MAPWIDTH):
                 # pg.draw.rect(SURFACE, colors[tilemap[row][column]],
-                # (column*TILESIZE, row*TILESIZE, TILESIZE, TILESIZE))
+                # (column*TILE_SIZE, row*TILE_SIZE, TILE_SIZE, TILE_SIZE))
                 self.SURFACE.blit(self.textures[self.tilemap[row][column]],
-                                  (row * self.TILESIZE, column * self.TILESIZE))
+                                  (row * self.TILE_SIZE, column * self.TILE_SIZE))
 
